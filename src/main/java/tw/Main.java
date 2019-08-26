5aefd85fa682da744efee401943ddc5e0dc71b23
caianmei <1610143991@qq.com>
2019-08-26 20:22:05 +0800

    get index

--- src/main/java/tw/Main.java
+++ src/main/java/tw/Main.java
@@ -1,5 +1,9 @@
 package tw;
 
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
+
 import tw.commands.GuessInputCommand;
 
 /**
@@ -8,10 +12,14 @@ import tw.commands.GuessInputCommand;
 public class Main {
 
     private static final int RUN_TIMES = 6;
-
+    
     public static void main(String[] args) throws Exception {
+    	GuessInputCommand guessInputCommand = new GuessInputCommand();
+    	Set<Integer> answer = guessInputCommand.getAnswer();
         for (int i = 0; i < RUN_TIMES; i++) {
-            String input = new GuessInputCommand().input();
+            String input = guessInputCommand.input();
+            String[] inputAnswer = guessInputCommand.stringToArray(input);
+            new GuessInputCommand().judgeAnswer(answer,inputAnswer);
             System.out.println(input);
         }
     }
--- src/main/java/tw/commands/GuessInputCommand.java
+++ src/main/java/tw/commands/GuessInputCommand.java
@@ -3,6 +3,9 @@ package tw.commands;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 /**
  * Created by jxzhong on 2017/5/19.
@@ -15,8 +18,57 @@ public class GuessInputCommand {
         bufferedReader = new BufferedReader(new InputStreamReader(System.in));
     }
 
+    public Set<Integer> getAnswer() {
+    	Set<Integer> set = new HashSet<>();
+		while (set.size() < 4) {
+			set.add(new Random().nextInt(9));
+		}
+		return set;
+	}
+    
     public String input() throws IOException {
         System.out.println("------Please input your answer as x x x x , x <10 ------");
         return bufferedReader.readLine();
     }
+    
+    public String[] stringToArray(String input) {
+		return input.split(" ");
+	}
+    
+    public int[] StringToInt(String[] inputStringAnswer) {
+		int[] inputAnswer = new int[inputStringAnswer.length];
+		for (int i = 0; i < inputStringAnswer.length; i++) {
+			inputAnswer[i] = Integer.valueOf(inputStringAnswer[i].trim());
+		}
+		return inputAnswer;
+ 	}
+
+    public boolean judgeIsExsit(Set<Integer> answer, int inputAnswerNumber) {
+		boolean flag = false;
+		for (Integer number : answer) {
+			if (number == inputAnswerNumber) {
+				flag = true;
+				break;
+			}
+		}
+		return flag;
+	}
+    
+    public int getIndex(Set<Integer> answer, int inputAnswerNumber) {
+		int index = -1;
+		int which = 0;
+		for (Integer number : answer) {
+			if (number == inputAnswerNumber) {
+				index = which;
+				break;
+			}
+			which ++;
+		}
+		return index;
+	}
+    
+	public String judgeAnswer(Set<Integer> answer, String[] inputStringAnswer) {
+		return null;
+		
+	}
 }
